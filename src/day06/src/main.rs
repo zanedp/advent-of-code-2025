41f5d9379f34d4e199025aabb428616b0e5d2c68
Zane Purvis <zanedp@users.noreply.github.com>
2025-12-07 13:59:52 -0500

    Add day 6, pt 2

--- src/day06/src/main.rs
+++ src/day06/src/main.rs
@@ -71,6 +71,7 @@ fn parse_input(input: &str) -> CephalopodMath {
 fn main() {
     let input = include_str!("sample-input.txt");
     // let input = include_str!("input.txt");
+    println!("--- Part 1 ---");
     let math = parse_input(input);
     let mut grand_total = 0;
     for column_index in 0..math.num_columns {
@@ -83,4 +84,80 @@ fn main() {
         grand_total += result;
     }
     println!("Grand total: {}", grand_total);
+
+    println!();
+    println!("--- Part 2 ---");
+    let chars: Vec<Vec<char>> = input
+        .lines()
+        .map(|line| line.chars().chain([' ', ' ', ' ', ' ']).collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+
+    let mut grand_total = 0;
+    let mut column_num = 0;
+    let mut cur_operator = None;
+    let mut cur_operands = Vec::new();
+    loop {
+        let cur_column_chars = get_column(&chars, column_num);
+        column_num += 1;
+        if cur_column_chars.is_empty() || cur_column_chars.iter().all(|ch| ch.is_whitespace()) {
+            // we got to the end of a math problem when there's no more data or all the characters are whitespace
+            // if we have an operator and some operands, we need to apply it
+            if cur_operator.is_none() && cur_operands.is_empty() {
+                // we got to the end of the input without finding any more data
+                break;
+            }
+            grand_total += match cur_operator {
+                Some(Operator::Add) => cur_operands.iter().sum::<u64>(),
+                Some(Operator::Multiply) => cur_operands.iter().product::<u64>(),
+                None => {
+                    panic!("No operator found");
+                    // break;
+                }
+            };
+            cur_operator = None;
+            cur_operands.clear();
+        } else {
+            let cur_number_chars = &cur_column_chars[..cur_column_chars.len() - 1];
+            let cur_number_as_string = String::from_iter(cur_number_chars);
+            let cur_number_as_string = cur_number_as_string.trim();
+
+            let operator_chars = &cur_column_chars[cur_column_chars.len() - 1..];
+            let operator_as_string = String::from_iter(operator_chars);
+            let operator_as_string = operator_as_string.trim();
+
+            if let Ok(num) = cur_number_as_string.parse::<u64>() {
+                cur_operands.push(num);
+            } else {
+                panic!(
+                    "Invalid character in column {column_num}: {:?}",
+                    cur_column_chars
+                );
+            }
+
+            if operator_as_string.is_empty() {
+                // most columns don't have operators at the end, so we just ignore them
+            } else if let Ok(op) = operator_as_string.parse::<Operator>() {
+                cur_operator = Some(op);
+            } else {
+                panic!(
+                    "Invalid character in column {column_num}: {:?}",
+                    cur_column_chars
+                );
+            }
+        }
+        if cur_column_chars.is_empty() {
+            break;
+        }
+    }
+    println!("Grand total: {}", grand_total);
+}
+
+fn get_column(chars: &Vec<Vec<char>>, column_index: usize) -> Vec<char> {
+    let mut column = Vec::new();
+    for row in chars {
+        if let Some(&c) = row.get(column_index) {
+            column.push(c);
+        }
+    }
+    column
 }
